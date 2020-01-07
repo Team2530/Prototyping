afa344796c893db3341ddb83a7a494bc8c5a90ec
Peter Marshall <pwmarshall@gmail.com>
2020-01-07 07:54:45 -0600

    First Init
    This should work as a simple one file code to run

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,40 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+// import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.VictorSP;
+// import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * This is a demo program showing the use of the RobotDrive class, specifically
+ * it contains the code necessary to operate a robot with tank drive.
+ */
+public class Robot extends TimedRobot {
+  // private DifferentialDrive m_myRobot;
+  private Joystick stick1;
+  // private Joystick stick2;
+  private VictorSP motor1;
+
+  @Override
+  public void robotInit() {
+    // m_myRobot = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
+    stick1 = new Joystick(0);
+    // stick2 = new Joystick(1);
+
+    motor1 = new VictorSP(0);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // m_myRobot.tankDrive(stick1.getY(), stick2.getY());
+    motor1.set(stick1.getY());
+  }
+}
